68907ec3bb5f96bda7cf5ed62eb39e35d5649c77
Quentin <hausser.quentin@gmail.com>
2020-05-15 00:51:33 +0200

    Disabled CSRF Protection (it's only an API) :saxophone:

--- mymind/urls.py
+++ mymind/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 import os
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.decorators.csrf import csrf_exempt
+
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^graphql$', GraphQLView.as_view(
-        graphiql=os.environ.get('DJANGO_DEBUG', True))),
+    url(r'^graphql$', csrf_exempt(GraphQLView.as_view(
+        graphiql=os.environ.get('DJANGO_DEBUG', True)))),
 ]
