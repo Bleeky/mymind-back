15d4a30eace5e0ca348d4d47ba2821267fe18f27
Quentin <hausser.quentin@gmail.com>
2020-05-17 16:50:49 +0200

    Mutation to get token :gift:

--- mymind/schema.py
+++ mymind/schema.py
@@ -6,13 +6,13 @@ import mymind.moods.schema
 
 
 class Query(mymind.moods.schema.Query, mymind.users.schema.Query, graphene.ObjectType):
-    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
-    verify_token = graphql_jwt.Verify.Field()
-    refresh_token = graphql_jwt.Refresh.Field()
+    pass
 
 
 class Mutation(mymind.users.schema.Mutation, graphene.ObjectType,):
-    pass
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
